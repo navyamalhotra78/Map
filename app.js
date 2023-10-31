875ce1bd5eb9a5f8fcd83acc826c9ca37528fa71
dikshapadi <dikshapadi@gmail.com>
2023-10-31 22:45:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 require("./config/dbConnection.js");
 
+// Routes
 app.use(mapRoutes);
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
