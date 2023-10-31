3d52f51c5ef0fa51174637164f4eebbdbff6443a
dikshapadi <dikshapadi@gmail.com>
2023-10-31 21:47:12 +0530

    remove line in model.js

--- model.js
+++ model.js
@@ -8,6 +8,5 @@ const stateInfoSchema = new mongoose.Schema({
   dance: String
 }, { collection: 'States' });
 
-const StateInfo = mongoose.model('StateInfo', stateInfoSchema);
 module.exports = mongoose.model('StateInfo', stateInfoSchema);
 
